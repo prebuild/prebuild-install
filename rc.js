1248473b2251a8e87c357f6767c8211e41341f9a
Victor Leach <victor96@mac.com>
2018-04-27 16:49:39 +0100

    Removed rc token, can be done through -e

--- rc.js
+++ rc.js
@@ -42,8 +42,7 @@ module.exports = function (pkg) {
     path: '.',
     proxy: env.npm_config_proxy || env['HTTP_PROXY'],
     'https-proxy': env.npm_config_https_proxy || env['HTTPS_PROXY'],
-    'local-address': env.npm_config_local_address,
-    token: process.env.GITHUB_TOKEN
+    'local-address': env.npm_config_local_address
   }, minimist(process.argv, {
     alias: {
       target: 't',
